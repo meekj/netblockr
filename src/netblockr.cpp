985389e81a1a214d0cfbbf04f80ef895be1ca844
meekj <meekjt@gmail.com>
2017-11-26 16:42:45 -0500

    Drop unused var declaration; Add copyright notice

--- src/netblockr.cpp
+++ src/netblockr.cpp
@@ -1,3 +1,20 @@
+// Copyright (C) 2017  Jon Meek
+//
+// This file is part of netblockr.
+//
+// netblockr is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// netblockr is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with netblockr.  If not, see <http://www.gnu.org/licenses/>.
+
 #include <Rcpp.h>
 using namespace Rcpp;
 
@@ -18,7 +35,6 @@ using namespace Rcpp;
 XPtr< nbTable > nbBuildNetblockTable (CharacterVector BaseAndMask, CharacterVector IPaddrStrings, IntegerVector Mask, CharacterVector Description) {
   struct sockaddr_in sa;
   const char* ipaddrstring;
-  char descstring;
   unsigned int binIP;
   
   int n = IPaddrStrings.size();
