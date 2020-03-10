cb65fc97afb515fe0b2c360cd1185515f2a7c4aa
meekj <meekjt@gmail.com>
2020-03-10 14:16:27 -0400

    Added nbReadNetwork(); Fixed namespace issues.

--- R/netblockr.R
+++ R/netblockr.R
@@ -25,15 +25,15 @@
 #'
 nbReadAndLoadNetwork <- function(file, skip_lines = 0) {
 
-    lines <- read_lines(file, skip = skip_lines) # Skip non-commented / non-indented header line(s) if needed
+    lines <- readr::read_lines(file, skip = skip_lines) # Skip non-commented / non-indented header line(s) if needed
 
-    t <- str_split_fixed(lines, '\\s+', 2)                   # Split on first whitespace
+    t <- stringr::str_split_fixed(lines, '\\s+', 2)                   # Split on first whitespace
 
     nets <- as.data.frame(t, stringsAsFactors = FALSE)
     names(nets) <- c('NetBlock', 'Description')
-    nets <- nets %>% filter(!str_detect(NetBlock, '#') & str_length(NetBlock) > 0) # Drop comments, empty & indented lines
+    nets <- nets %>% dplyr::filter(!stringr::str_detect(NetBlock, '#') & stringr::str_length(NetBlock) > 0) # Drop comments, empty & indented lines
 
-    t <- str_split_fixed(nets$NetBlock, '/', 2)              # Breakout base IP address and number of bits in mask
+    t <- stringr::str_split_fixed(nets$NetBlock, '/', 2)              # Breakout base IP address and number of bits in mask
     nets$Base <- t[,1]
     nets$Bits <- as.integer(t[,2])
 
@@ -50,9 +50,9 @@ nbReadAndLoadNetwork <- function(file, skip_lines = 0) {
 #'
 nbLoadNetwork <- function(nets) {
 
-    nets <- nets %>% filter(!str_detect(NetBlock, '#') & str_length(NetBlock) > 0) # Drop comments, empty & indented lines
+    nets <- nets %>% dplyr::filter(!str_detect(NetBlock, '#') & dplyr::str_length(NetBlock) > 0) # Drop comments, empty & indented lines
 
-    t <- str_split_fixed(nets$NetBlock, '/', 2)              # Breakout base IP address and number of bits in mask
+    t <- stringr::str_split_fixed(nets$NetBlock, '/', 2)              # Breakout base IP address and number of bits in mask
     nets$Base <- t[,1]
     nets$Bits <- as.integer(t[,2])
 
@@ -60,3 +60,29 @@ nbLoadNetwork <- function(nets) {
     nbSetMaskOrder(nb_ptr, sort(unique(nets$Bits), decreasing = TRUE))                    # Add mask search order - Required
     return(nb_ptr)
 }
+
+#' Build a data frame from a network description file for later loading with nbLoadNetwork()
+#' Used when multiple network description files need to be combined
+#' Note that there is currently no check for duplicate NetBlocks, that can be done externally
+#'
+#' #' @param file Path to the input file
+#' @param skip_lines Optional number of lines to skip, use if there is an uncommented header
+#' @return A data frame appropriate for nbLoadNetwork()
+#' The input can contain empty rows. A '#' in the NetBlock field will cause that row to be ignored.
+#'
+nbReadNetwork <- function(file, skip_lines = 0) { # New, Feb 2020
+
+    lines <- readr::read_lines(file, skip = skip_lines) # Skip non-commented / non-indented header line(s) if needed
+
+    t <- stringr::str_split_fixed(lines, '\\s+', 2)     # Split on first whitespace
+
+    nets <- as.data.frame(t, stringsAsFactors = FALSE)
+    names(nets) <- c('NetBlock', 'Description')
+    nets <- nets %>% dplyr::filter(!stringr::str_detect(NetBlock, '#') & stringr::str_length(NetBlock) > 0) # Drop comments, empty & indented lines
+
+    t <-  stringr::str_split_fixed(nets$NetBlock, '/', 2) # Breakout base IP address and number of bits in mask
+    nets$Base <- t[,1]
+    nets$Bits <- as.integer(t[,2])
+    return(nets)
+}
+
